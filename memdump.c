8630b1d9640879cc732a043038deefee5c4ada82
rjkall <rj@notme.com>
2013-08-14 19:12:49 -0700

    finds heap values for a PID

--- memdump.c
+++ memdump.c
@@ -31,44 +31,46 @@ int attach_pid(int pid_id)
 
 int find_heap_values(int pid_id)
 {
-	struct heap *heaps = malloc(sizeof(struct heap*));
+	struct heap **heaps = (struct heap**)malloc(sizeof(struct heap**));
 	int heap_size = 0;	
 	int i;
 	char *end;
-	char *pid_map;
+	char pid_map[30];
 	char *heap_loc;
 	char *heap_end;
-	char data[200];
+	char data[1024];
 	FILE *map;
-
-	snprintf(pid_map,(sizeof(pid_id)/sizeof(int)),"/proc/%d/maps",pid_id);
+	
+	sprintf(pid_map,"/proc/%d/maps",pid_id);
 	map = fopen(pid_map,"r");
 	if(map == NULL) {
 		printf("* Error, unable to read process %d file map.\n",pid_id);
 		return -1;
-	} else {
-		printf("* Attempting to find process %d heap location.\n",pid_id);
 	}
-
-	while(fgets(data, 200, map) != NULL) {
+ 
+	printf("* Attempting to find process %d heap location.\n",pid_id);
+	while(fgets(data, 1024, map) != NULL) {
 		if(strstr(data, "[heap]") != NULL) {
-			heaps[heap_size] = (struct heap *)malloc(sizeof(struct heap));
+			heaps[heap_size] = (struct heap*)malloc(sizeof(struct heap*));
 			heap_loc = strtok(data,"-");
 			if((strcmp(heap_loc,"")) == 0) {
 				printf("* Error finding heap addresses.\n");
 				return -1;
 			}
 
-			heaps[heap_size].saddress = (void *)strtol(heap_loc,&end,16);
-			heap_loc = strtok(data,"-");
-			heap_loc = strtok(NULL,"-");
-			heap_end = strtok(heap_loc," ");
-			heaps[heap_size].address = (void *)strtol(heap_end,&end,16);
-			heaps[heap_size].size = (int)((void*)strtol(heaps[heap_size].saddress,&end,16) - heaps[heap_size].address,&end,16);
-			heaps = realloc(heaps, heap_size++ * sizeof(struct heap));
+			heaps[heap_size]->saddress = (void *)strtol(heap_loc,&end,16);
+			printf("[heap] start:0x%s ",heap_loc);
+			heap_loc = strtok(NULL,"- ");
+			printf("end:0x%s ",heap_loc);
+			heaps[heap_size]->address = (void *)strtol(heap_loc,&end,16);
+			heaps[heap_size]->size = (int)(heaps[heap_size]->address - heaps[heap_size]->saddress);
+			printf("size=%d\n",heaps[heap_size]->size);
+			heap_size++;
+			heaps = realloc(heaps, heap_size*sizeof(struct heap*));
 		}
 	}
 
+	fclose(map);
 	for(i=0; i<heap_size; i++) {
 		free(heaps[i]);
 	}
