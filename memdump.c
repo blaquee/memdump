3b07c47ed731400cc5b2bd3b54e91cb7dffd5828
rjkall <rj@notme.com>
2013-08-14 20:46:46 -0700

    error checking, fixes and cleaned up code

--- memdump.c
+++ memdump.c
@@ -13,7 +13,8 @@ struct heap
 	int size;
 };
 
-long peekdata(void *addr, int pid) {
+long peekdata(void *addr, int pid) 
+{
   return ptrace(PTRACE_PEEKDATA, pid, addr, NULL);
 }
 
@@ -29,33 +30,42 @@ int attach_pid(int pid_id)
 	}
 }
 
-int find_heap_values(int pid_id)
+int dump_heap(struct heap **heap_dump, int pid_id)
+{
+	if(attach_pid(pid_id) == 0) {
+		printf("* Error attaching to process [%d].\n",pid_id);
+		return 1;
+	}
+
+	
+	return 0;
+}
+
+struct heap** find_heap_values(int pid_id)
 {
 	struct heap **heaps = (struct heap**)malloc(sizeof(struct heap**));
-	int heap_size = 0;	
-	int i;
+	int i,heap_size = 0;	
 	char *end;
 	char pid_map[30];
-	char *heap_loc;
-	char *heap_end;
+	char *heap_loc, *heap_end;
 	char data[1024];
 	FILE *map;
 	
 	sprintf(pid_map,"/proc/%d/maps",pid_id);
 	map = fopen(pid_map,"r");
 	if(map == NULL) {
-		printf("* Error, unable to read process %d file map.\n",pid_id);
-		return -1;
+		printf("* Error, unable to read process [%d] file map.\n",pid_id);
+		exit(1);
 	}
  
-	printf("* Attempting to find process %d heap location.\n",pid_id);
+	printf("* Attempting to find process [%d] heap location(s).\n",pid_id);
 	while(fgets(data, 1024, map) != NULL) {
 		if(strstr(data, "[heap]") != NULL) {
 			heaps[heap_size] = (struct heap*)malloc(sizeof(struct heap*));
 			heap_loc = strtok(data,"-");
 			if((strcmp(heap_loc,"")) == 0) {
 				printf("* Error finding heap addresses.\n");
-				return -1;
+				exit(1);
 			}
 
 			heaps[heap_size]->saddress = (void *)strtol(heap_loc,&end,16);
@@ -75,13 +85,30 @@ int find_heap_values(int pid_id)
 		free(heaps[i]);
 	}
 	free(heaps);
-
+	return heaps;
 }
 
 
 int main(int argc, char *argv[])
 {
-	find_heap_values(atoi(argv[1]));	
+	struct heap **heap_dump = NULL;
+	int proc_pid = 0;
 
+        printf("memory dumper version 1.0.1\n");
+        printf("--------------------------------------------------------------\n");
+
+	if(argc != 2) {
+		printf("usage %s PID\n",argv[0]);
+		exit(1);
+	} else {
+		proc_pid = atoi(argv[1]);
+	}
+
+	heap_dump = find_heap_values(proc_pid);
+	if(heap_dump != NULL) {
+		dump_heap(heap_dump,proc_pid);
+	} else {
+		printf("* Unknown error occured.\n");
+	}
 	return 0;
 }	
